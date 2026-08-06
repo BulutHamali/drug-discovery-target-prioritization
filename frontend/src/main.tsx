6e0ce00df4b09be20f3e10e3b911947ce42446ce
Bulut Hamali <buluthamali@gmail.com>
2026-08-05 20:03:25 -0400

    Clarify target explorer data state

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,5 +4,6 @@ import App from "./App";
 import "./styles.css";
 import "./target-explorer.css";
 import "./aws-branding.css";
+import "./target-state.css";
 
 createRoot(document.getElementById("root")!).render(<StrictMode><App /></StrictMode>);
